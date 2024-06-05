6ba4675345c06bff8cd4de185003424972a2b1b9
storkbison <antonbukas53@gmail.com>
2024-06-05 11:18:43 +0900

    tools

---
+++ mstart.bat
@@ -0,0 +1,2 @@
+
+hashcat.exe -m 26600 --keep-guessing ./test.hash -r rules/best64.rule example.dict
---
+++ start.bat
@@ -0,0 +1,2 @@
+
+hashcat.exe -m 23100 --keep-guessing ./test-local.hash -r rules/rockyou-30000.rule example.dict
